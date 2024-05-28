7ab584ca25ed70fd6915bef5edc82178af481c3b
Yorman Balan <yormanmp3balan2@gmail.com>
2024-05-28 16:30:36 -0400

    Estructura del proyecto armada

---
+++ src/Catalogo.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Button} from 'flowbite-react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
