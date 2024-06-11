dbd2077e5f23a93faa13ca92cf5f94399e0195fb
htovar21 <tovarfigueroa@gmail.com>
2024-06-10 20:52:47 -0400

    primer commit

--- src/components/Testimonios.jsx
+++ src/components/Testimonios.jsx
@@ -1,7 +1,111 @@
-import React from 'react'
+import React from "react";
+import "flowbite";
+import img2 from "../assets/img2.jpeg";
 
 export default function Testimonios() {
   return (
-    <div>Testimonios</div>
-  )
+    <div className="relative flex bg-clip-border bg-white text-gray-700  w-full max-w-[64rem] flex-row">
+      <div className="relative w-2/5 m-0 overflow-hidden text-gray-700 bg-white rounded-r-none   shrink-0">
+        <img
+          src={img2}
+          alt="card-image"
+          className="object-cover w-full h-full"
+        />
+        <svg
+          class="w-6 h-6 text-gray-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+        </svg>
+      </div>
+      <div className="p-6">
+        <h4 className="text-center block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-[#b2559b]">
+          Testimonios
+        </h4>
+        <p className="px-20 text-center block mb-8 font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+          No lo decimos nosotros, lo dicen nuestros clientes Inmobiliaria M&R tu
+          asesor de confianza
+        </p>
+        <div className="bg-clip-border rounded-xl bg-white shadow-lg w-full max-w-[48rem] p-6">
+          <div className="relative flex">
+            <div className="w-1/12 flex items-center"></div>
+            <div className="w-11/12">
+              <p className="font-sans text-base antialiased font-normal leading-relaxed text-gray-700">
+                Excelente asesor inmobiliario. Responsabilidad, profesionalismo
+                y compromiso, 100% recomendados y satisfecha con su labor. Mil
+                Gracias M/R. Queremos expresar nuestras gracias por ser nuestro
+                agente inmobiliario.
+              </p>
+              <p className="font-sans text-base antialiased font-normal leading-relaxed text-[#b2559b]">
+                - Vanesa Guzmán
+              </p>
+              <div className=" mx-6 flex justify-end">
+                <svg
+                  class="w-6 h-6 text-[#b2559b] dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                </svg>
+
+                <svg
+                  class="w-6 h-6 text-[#b2559b] dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                </svg>
+                <svg
+                  class="w-6 h-6 text-[#b2559b] dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                </svg>
+                <svg
+                  class="w-6 h-6  text-[#b2559b] dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                </svg>
+                <svg
+                  class="w-6 h-6 text-[#b2559b] dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M13.849 4.22c-.684-1.626-3.014-1.626-3.698 0L8.397 8.387l-4.552.361c-1.775.14-2.495 2.331-1.142 3.477l3.468 2.937-1.06 4.392c-.413 1.713 1.472 3.067 2.992 2.149L12 19.35l3.897 2.354c1.52.918 3.405-.436 2.992-2.15l-1.06-4.39 3.468-2.938c1.353-1.146.633-3.336-1.142-3.477l-4.552-.36-1.754-4.17Z" />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
