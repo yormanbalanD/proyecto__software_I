3b99d163a09f0885b279b416d763c814c07b3792
LorenzoParra <115849884+Lorenzo36036@users.noreply.github.com>
2024-06-12 20:33:54 -0400

    finalizado
    finalizado

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,18 +6,13 @@ import PorqueElegirnos from './components/PorqueElegirnos'
 import NuestroEquipo from './components/NuestroEquipo'
 import Testimonios from './components/Testimonios'
 import Footer from './components/Footer'
-
+import ExperienciaProfesionalidad from './components/Experiencia&Profesionalidad'
 
 export default function Home() {
   return (
     <div className='w-full min-h-screen h-auto'>
-        <Hero></Hero>
-        <Destacados></Destacados>
-        <NuestrosServicios></NuestrosServicios>
-        <PorqueElegirnos></PorqueElegirnos>
-        <NuestroEquipo></NuestroEquipo>
-        <Testimonios></Testimonios>
-        <Footer />
+      <ExperienciaProfesionalidad/>
+
     </div>
   )
 }
