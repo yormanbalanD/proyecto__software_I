3b1796f8bc246af01126544d263ea06f30cb6cc2
Yorman Balan <yormanmp3balan2@gmail.com>
2024-07-04 12:19:19 -0400

    Se agrego className a Input

--- src/components/formularios/Input.jsx
+++ src/components/formularios/Input.jsx
@@ -1,14 +1,14 @@
 import React, { useId, useState } from 'react'
 import { LuEye, LuEyeOff } from "react-icons/lu"
 
-export default function Input({ label, icon, type, disabled }) {
+export default function Input({ label, icon, type, disabled, className }) {
     const [visible, setVisible] = useState(false)
     const [value, setValue] = useState('')
     const [id, setId] = useState(useId())
 
     if (type == 'password') {
         return (
-            <div className={disabled ? 'flex items-center border-b-2 border-black py-1 gap-2 opacity-65 cursor-not-allowed *:cursor-not-allowed': 'flex items-center border-b-2 border-black py-1 gap-2'}>
+            <div className={disabled ? `flex items-center border-b-2 border-black py-1 gap-2 opacity-65 cursor-not-allowed *:cursor-not-allowed ${className}` : `flex items-center border-b-2 border-black py-1 gap-2 ${className}`}>
                 <label htmlFor={id}>{icon}</label>
                 {visible && <input type='text' id={id} onChange={(e) => setValue(e.target.value)} value={value} placeholder={label} className='w-full py-1 px-2 appearance-none border-0 outline-none focus:ring-0 duration-75' disabled={disabled} />}
                 {!visible && <input type='password' id={id} onChange={(e) => setValue(e.target.value)} value={value} placeholder={label} className='w-full py-1 px-2 appearance-none border-0 outline-none focus:ring-0 duration-75' disabled={disabled} />}
@@ -17,7 +17,7 @@ export default function Input({ label, icon, type, disabled }) {
         )
     } else {
         return (
-            <div className={disabled ? 'flex items-center border-b-2 border-black py-1 gap-2 opacity-65 cursor-not-allowed *:cursor-not-allowed': 'flex items-center border-b-2 border-black py-1 gap-2'}>
+            <div className={disabled ? `flex items-center border-b-2 border-black py-1 gap-2 opacity-65 cursor-not-allowed *:cursor-not-allowed ${className}` : `flex items-center border-b-2 border-black py-1 gap-2 ${className}`}>
                 <label htmlFor={id}>{icon}</label>
                 <input id={id} type={type} placeholder={label} onChange={() => setValue(value)} value={value} className='w-full py-1 px-2 appearance-none border-0 outline-none focus:ring-0 duration-75' disabled={disabled} />
             </div>
