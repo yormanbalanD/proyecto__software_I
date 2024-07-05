6ce9a8befe63d466bdee32594cba00148dd8709e
Yorman Balan <yormanmp3balan2@gmail.com>
2024-07-04 21:25:42 -0400

    React Hook Form agregado al login y signup

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,29 +1,53 @@
 import React from "react";
 import { useState } from "react";
 
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import ErrorMessage from "./formularios/ErrorMessage";
+
 import Input from "./formularios/Input";
 import { FaLock } from "react-icons/fa6"
 import { MdEmail } from "react-icons/md"
 
+const SchemaLogin = z.object({
+  email: z.string({
+    required_error: 'Introduzca el Correo Electronico',
+  }).email({
+    message: 'El Correo Electronico ingresado no es valido',
+  }),
+  password: z.string({
+    required_error: 'Introduzca la Contraseña',
+  }).min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
+})
+
 const LoginForm = ({ value, onChange }) => {
-  const [password, setPassword] = useState('');
-  const [usuario, setUsuario] = useState('');
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
+    resolver: zodResolver(SchemaLogin)
+  })
+
+  const onSubmit = (data) => console.log(data)
 
   return (
-    <form className="px-8 py-4 flex flex-col justify-center h-full w-full max-w-md mx-auto">
+    <form onSubmit={handleSubmit(onSubmit)} className="px-8 py-4 flex flex-col justify-center h-full w-full max-w-md mx-auto">
       <img src="./public/LogoM&R.png" className="w-32 m-2 mb-10 flex self-center" />
-      <Input className={'mb-5'} type='text' icon={<MdEmail className="text-3xl"/>} label={'Usuario'} value={usuario} onChange={setUsuario} />
-      <Input className={'mb-5'} type='password' icon={<FaLock className="text-2xl"/>} label={'Contraseña'} value={password} onChange={setPassword} />
+      <div className={'mb-5'}>
+        <Input type='text' icon={<MdEmail className="text-3xl" />} label={'Correo Electronico'} register={register('email')} name={'email'} />
+        {errors.email && <ErrorMessage value={errors.email.message} />}
+      </div>
+      <div className={'mb-5'}>
+        <Input type='password' icon={<FaLock className="text-2xl" />} label={'Contraseña'} register={register('password')} name={'password'} />
+        {errors.password && <ErrorMessage value={errors.password.message} />}
+      </div>
       <div className="flex items-center justify-between mb-10 mt-2">
         <div className="flex items-center gap-2">
           <input type="checkbox" className="rounded checked:bg-morado focus:ring-morado" />
           <span>Recordar</span>
         </div>
-        {/* <a href="#" className="text-black text-sm hover:text-rosado">
-          ¿Has olvidado tu contraseña?
-        </a> */}
       </div>
-      <button className="bg-rosado active:shadow-none text-white px-4 py-2 rounded-lg shadow-xl hover:bg-morado">
+      <button type="submit" className="bg-rosado active:shadow-none text-white px-4 py-2 rounded-lg shadow-xl hover:bg-morado">
         Iniciar sesión
       </button>
     </form>
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { useState } from "react";
 
 import Input from '../components/formularios/Input';
-// import ContainerInput from "./ContainerInput";
-import PasswordButton from "./PasswordButton";
+import ErrorMessage from '../components/formularios/ErrorMessage';
 import { z } from "zod";
 import { useForm } from "react-hook-form";
-import  { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 import { FaLock, FaP } from "react-icons/fa6"
 import { FaUser } from "react-icons/fa"
@@ -17,19 +16,38 @@ import { FaPencilAlt } from "react-icons/fa"
 
 
 const SchemaSingup = z.object({
-  nombres: z.string(),
-  apellidos: z.string(),
-  numeroDeDocumento: z.string(),
-  telefono: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-  confirmPassword: z.string()
+  nombres: z.string({
+    required_error: 'Introduzca el Nombre',
+  }).min(3, {
+    message: 'Los Nombres debe tener al menos 3 caracteres',
+  }),
+  apellidos: z.string({
+    required_error: 'Introduzca los Apellidos',
+  }).min(3, {
+    message: 'Los Apellidos deben tener al menos 3 caracteres',
+  }),
+  numeroDeDocumento: z.string({
+    required_error: 'Introduzca el Numero de Documento',
+  }).min(7, {
+    message: 'El Numero de Documento debe tener al menos 7 caracteres',
+  }),
+  telefono: z.string().min(11, {
+    message: 'El telefono debe tener al menos 11 caracteres',
+  }),
+  email: z.string({
+    required_error: 'Introduzca el Correo Electronico',
+  }).email({
+    message: 'El Correo Electronico ingresado no es valido',
+  }),
+  password: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  }),
+  confirmPassword: z.string().min(8, {
+    message: 'La contraseña debe tener al menos 8 caracteres',
+  })
 })
 
 const SingUpForm = () => {
-  // const { register, handleSubmit, watch, formState: { errors } } = useForm({
-  //   resolver: zodResolver(SchemaSingup)
-  // })
   const { register, handleSubmit, watch, formState: { errors } } = useForm({
     resolver: zodResolver(SchemaSingup)
   })
@@ -55,28 +73,40 @@ const SingUpForm = () => {
       {/* form */}
 
       <div className="grid grid-cols-2 gap-5">
-        <Input icon={<FaUser className="text-2xl"/>} label={'Nombre'} type={'text'} register={register('nombres')} name={'nombres'} />
-        <Input icon={<FaPencilAlt className="text-2xl"/>} label={'Apellidos'} type={'text'} register={register('apellidos')} name={'apellidos'}  />
+        <div>
+          <Input icon={<FaUser className="text-2xl" />} label={'Nombre'} type={'text'} register={register('nombres')} name={'nombres'} />
+          {errors.nombres && <ErrorMessage value={errors.nombres.message} />}
+        </div>
 
-        {/* <div className="mb-3 relative border-b border-gray-400">
-          <label htmlFor="tipoDocumento" className="flex text-gray-500 absolute p-3">
-            <svg className="w-6 h-6 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="M6 16v-3h.375a.626.626 0 0 1 .625.626v1.749a.626.626 0 0 1-.626.625H6Zm6-2.5a.5.5 0 1 1 1 0v2a.5.5 0 0 1-1 0v-2Z" /><path fillRule="evenodd" d="M11 7V2h7a2 2 0 0 1 2 2v5h1a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-1a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2H3a1 1 0 0 1-1-1v-9a1 1 0 0 1 1-1h6a2 2 0 0 0 2-2Zm7.683 6.006 1.335-.024-.037-2-1.327.024a2.647 2.647 0 0 0-2.636 2.647v1.706a2.647 2.647 0 0 0 2.647 2.647H20v-2h-1.335a.647.647 0 0 1-.647-.647v-1.706a.647.647 0 0 1 .647-.647h.018ZM5 11a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h1.376A2.626 2.626 0 0 0 9 15.375v-1.75A2.626 2.626 0 0 0 6.375 11H5Zm7.5 0a2.5 2.5 0 0 0-2.5 2.5v2a2.5 2.5 0 0 0 5 0v-2a2.5 2.5 0 0 0-2.5-2.5Z" clipRule="evenodd" /><path d="M9 7V2.221a2 2 0 0 0-.5.365L4.586 6.5a2 2 0 0 0-.365.5H9Z" /></svg>
-          </label>
-          <select id="seleccion" name="seleccion" placeholder="Tipo de documento" className="block w-full p-3 pl-10 rounded-md bg-transparent border-none focus:ring-0 outline-none" required>
-            {options.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </div> */}
-        <Input icon={<HiIdentification className="text-3xl"/>} label='Numero de Documento' type='text' register={register('numeroDeDocumento')} name={'numeroDeDocumento'} />
+        <div>
+          <Input icon={<FaPencilAlt className="text-2xl" />} label={'Apellidos'} type={'text'} register={register('apellidos')} name={'apellidos'} />
+          {errors.apellidos && <ErrorMessage value={errors.apellidos.message} />}
+        </div>
 
-        <Input icon={<FaPhoneAlt className="text-2xl"/>} label='Telefono' type={'tel'} register={register('telefono')} name={'telefono'} />
-        <Input icon={<MdEmail className="text-3xl"/>} label={'Correo Electronico'} type={'email'} register={register('email')} name={'email'} />
+        <div>
+          <Input icon={<HiIdentification className="text-3xl" />} label='Numero de Documento' type='text' register={register('numeroDeDocumento')} name={'numeroDeDocumento'} />
+          {errors.numeroDeDocumento && <ErrorMessage value={errors.numeroDeDocumento.message} />}
+        </div>
 
-        <Input icon={<FaLock className="text-2xl"/>} label={'Contraseña'} type={'password'} register={register('password')} name={'password'} />
-        <Input icon={<FaLock className="text-2xl"/>} label={'Confirmar Contraseña'} type={'password'} register={register('confirmPassword')} name={'confirmPassword'} />
+        <div>
+          <Input icon={<FaPhoneAlt className="text-2xl" />} label='Telefono' type={'tel'} register={register('telefono')} name={'telefono'} />
+          {errors.telefono && <ErrorMessage value={errors.telefono.message} />}
+        </div>
+
+        <div>
+          <Input icon={<MdEmail className="text-3xl" />} label={'Correo Electronico'} type={'email'} register={register('email')} name={'email'} />
+          {errors.email && <ErrorMessage value={errors.email.message} />}
+        </div>
+
+        <div>
+          <Input icon={<FaLock className="text-2xl" />} label={'Contraseña'} type={'password'} register={register('password')} name={'password'} />
+          {errors.password && <ErrorMessage value={errors.password.message} />}
+        </div>
+
+        <div>
+          <Input icon={<FaLock className="text-2xl" />} label={'Confirmar Contraseña'} type={'password'} register={register('confirmPassword')} name={'confirmPassword'} />
+          {errors.confirmPassword && <ErrorMessage value={errors.confirmPassword.message} />}
+        </div>
 
         <div className="mb-3 col-span-2 flex items-center">
           <input type="checkbox" id="acepto" name="acepto" className="block w-h-5 rounded-md focus:outline-none focus:ring-morado pr-2 checked:bg-morado" required />
