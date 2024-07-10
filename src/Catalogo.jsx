7265043ae047c3571f74d0243beab21cf75aea0a
Jenorg <jenfermz44@gmail.com>
2024-07-10 14:25:24 -0400

    feat: :sparkles: Added loader

--- src/Catalogo.jsx
+++ src/Catalogo.jsx
@@ -6,62 +6,83 @@ import ContainerCatalogo from "./components/catalogo/ContainerCatalogo"
 import AsideCatalogo from "./components/catalogo/AsideCatalogo"
 import Nav from "./components/Nav"
 import SearchBar from './components/catalogo/SearchBar'
+import Loader from './components/Loader'
 
 export default function App() {
   const [viviendas, setViviendas] = useState([])
+  const [isLoading, setIsLoading] = useState([])
 
   const getViviendas = async () => {
-    const request = await fetch(`${URLBACKEND}/api/estate`, {
-      method: 'GET',
-      headers: {
-        'ngrok-skip-browser-warning': 'true'
+    setIsLoading(true)
+    try {
+      const request = await fetch(`${URLBACKEND}/api/estate`, {
+        method: 'GET',
+        headers: {
+          'ngrok-skip-browser-warning': 'true'
+        }
+      })
+  
+      if (request.status == 200) {
+        const response = await request.json()
+        setViviendas(response.data)
+        console.log(response.data)
+      } else {
+        console.log(request)
       }
-    })
-
-    if (request.status == 200) {
-      const response = await request.json()
-      setViviendas(response.data)
-      console.log(response.data)
-    } else {
-      console.log(request)
+    } catch (error) {
+      console.error("ERROR: " + error)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   const getViviendasByFilters = async (filters) => {
-    const request = await fetch(`${URLBACKEND}/api/estate/byfilters`, {
-      method: 'POST',
-      body: JSON.stringify(filters),
-      headers: {
-        'content-type': 'application/json',
-        'ngrok-skip-browser-warning': 'true'
+    setIsLoading(true)
+    try {
+      const request = await fetch(`${URLBACKEND}/api/estate/byfilters`, {
+        method: 'POST',
+        body: JSON.stringify(filters),
+        headers: {
+          'content-type': 'application/json',
+          'ngrok-skip-browser-warning': 'true'
+        }
+      })
+  
+      if (request.status == 200) {
+        const response = await request.json()
+        setViviendas(response.data)
+      } else {
+        console.log(request)
       }
-    })
-
-    if (request.status == 200) {
-      const response = await request.json()
-      setViviendas(response.data)
-    } else {
-      console.log(request)
+    } catch (error) {
+      console.error(`ERROR: ${error}`)
+    } finally {
+      setIsLoading(false)
     }
   }
 
   const getViviendasByName = async (name) => {
-    const request = await fetch(`${URLBACKEND}/api/estate/searchbyname`, {
-      method: 'POST',
-      body: JSON.stringify({
-        name: name
-      }),
-      headers: {
-        'content-type': 'application/json',
-        'ngrok-skip-browser-warning': 'true'
+    setIsLoading(true)
+    try {
+      const request = await fetch(`${URLBACKEND}/api/estate/searchbyname`, {
+        method: 'POST',
+        body: JSON.stringify({
+          name: name
+        }),
+        headers: {
+          'content-type': 'application/json',
+          'ngrok-skip-browser-warning': 'true'
+        }
+      })
+  
+      if (request.status == 200) {
+        const response = await request.json()
+        setViviendas(response.data)
+      } else {
+        console.log(request)
       }
-    })
-
-    if (request.status == 200) {
-      const response = await request.json()
-      setViviendas(response.data)
-    } else {
-      console.log(request)
+    } catch (error) {
+      console.error(`ERROR: ${error}`)
     }
   }
 
@@ -76,7 +97,9 @@ export default function App() {
         <SearchBar requestByName={getViviendasByName} />
         <div className="flex mt-14 w-full">
           <AsideCatalogo requestFilters={getViviendasByFilters} />
-          <ContainerCatalogo viviendas={viviendas} />
+          {isLoading ? 
+            (<Loader />)
+            : <ContainerCatalogo viviendas={viviendas} />}
         </div>
       </main>
     </div>
